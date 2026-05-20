81ee5f0cd2e6f4e6cd743baf30933036ca886a3e
AxelGD2 <axelenriquecerritos@gmail.com>
2026-05-20 10:20:01 -0600

    Initial commit

---
+++ src/main/java/CECT_Conexion/CECTConexion.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package CECT_Conexion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author axele
+ */
+public class CECTConexion {
+    private Connection con;
+
+    public CECTConexion() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            this.con = DriverManager.getConnection("jdbc:mysql://localhost:3306/motocicletas?useSSL=false&serverTimezone=UTC", "root", "");
+            
+        } catch (Exception e) {
+            System.out.println("Error");
+            return;
+        }
+        System.out.println("Conectado");
+    }
+    
+    public Connection getCon() {
+        return con;
+    }
+}
